d57fc7393118278c47207841a1bc1bcb4a49b95d
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-12 09:50:43 +0530

    1 file modified

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let body = document.querySelector('#gradient');
 function setGradient() {
   body.style.background =
     'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
+    css.textContent = body.style.background + ';';
 }
 
 color1.addEventListener('input', setGradient);
