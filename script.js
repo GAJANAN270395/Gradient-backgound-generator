1b0193bcfd6f241018843c0fc7b6a5bed06c6a15
GAJANAN270395 <108356887+GAJANAN270395@users.noreply.github.com>
2023-02-12 10:26:36 +0530

    3 files modified

--- script.js
+++ script.js
@@ -2,12 +2,23 @@ let css = document.querySelector('h3');
 let color1 = document.querySelector('.color1');
 let color2 = document.querySelector('.color2');
 let body = document.querySelector('#gradient');
+let copyButton = document.querySelector('#copy');
 
 function setGradient() {
   body.style.background =
     'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
     css.textContent = body.style.background + ';';
 }
+if(css.textContent == ''){
+  copyButton.style.display = 'none';
+}
+
+copyButton.addEventListener('click', ()=>{
+  let copyCss = css.textContent;
+  
+  navigator.clipboard.writeText(copyCss);
+  alert('CSS Copied'+ copyCss);
+});
 
 color1.addEventListener('input', setGradient);
 color2.addEventListener('input', setGradient);
